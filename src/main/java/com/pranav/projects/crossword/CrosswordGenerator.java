8d06b8a47e010e53bbcd6596a3bbb20abb89e045
Pranav Gupta <partholikesit@gmail.com>
2013-03-31 23:21:01 +0530

    Added CrosswordGenerator.java

---
+++ src/main/java/com/pranav/projects/crossword/CrosswordGenerator.java
@@ -0,0 +1,26 @@
+package com.pranav.projects.crossword;
+
+public class CrosswordGenerator {
+	private int initialPopulationSize;
+	public int getInitialPopulationSize(){
+		return initialPopulationSize;
+	}
+	
+	/**
+	 * Todo
+	 * get input arguments and set initial parameters
+	 * generate initial population
+	 * loop:
+	 * 		selection
+	 * 		variation
+	 * 		survival
+	 * return best individual
+	 */
+	public static void main(String args[]){
+		
+	}
+	
+	public static void genInitialPopulation(){
+		
+	}
+}
